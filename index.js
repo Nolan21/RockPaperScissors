821bf196184ff87aee1355eeaf4b418544cbb057
Nolan Kislauskis <nolan5652@gmail.com>
2022-04-03 22:56:58 -0400

    Solid version with 1 round working. Need to figure out how to loop 5 games. Need to call for user input at the end of each loop, or the beginning of each iteration

--- index.js
+++ index.js
@@ -10,18 +10,16 @@
 // then use console.log() to display results of each round and the winner at the end
 // use prompt() to get input from the user
 
-function computerPlay (){ //returns a random selection from myArray which consists of R, P, or S...
-    let myArray = ["rock", "paper", "scissors"]; //stores strings rock, paper, scissors in myArray
-    let computerSelection = myArray[Math.floor(Math.random()*myArray.length)]; //selects a random string inside of myArray and stores it in computerSelection
-    return computerSelection;
-}
-
-function userPlay () {
+function userPlay () { //prompt user for input of 3 options: rock, paper, or scissors & returns their choice, case in-sensitive
 let userChoice = window.prompt("Please type your selection of Rock, Paper, or Scissors: ");
-    return userChoice.toLowerCase(); // converts input into all lowercase
+    return userChoice.toLowerCase(); // converts input into all lowercase, making it so input is case-insensitive
 }
-
-function playRound(playerSelection, computerSelection){ //function to determine the winner of a round based on real rock/paper/scissors rules. Contains all possible outcomes
+function computerPlay (){ //returns a random selection from myArray which consists of rock, paper, or scissors.
+    let myArray = ["rock", "paper", "scissors"]; //stores strings rock, paper, scissors in an array called myArray
+    let computerSelection = myArray[Math.floor(Math.random()*myArray.length)]; //selects a random string inside of myArray and stores it in computerSelection
+    return computerSelection; 
+}
+function determineWinner(playerSelection, computerSelection){ //function to determine the winner of a round based on real rock/paper/scissors rules. Contains all possible outcomes
     if ((playerSelection == "rock") && (computerSelection == "rock")){
         return "Tie game!"
     } else if ((playerSelection == "rock") && (computerSelection == "paper")){
@@ -44,16 +42,14 @@ function playRound(playerSelection, computerSelection){ //function to determine 
         return "Tie game!"
     } 
 }
-
 const playerSelection = userPlay(); //stores result of userPlay() inside variable called playerSelection
 const computerSelection = computerPlay(); //stores result of computerPlay() inside variable called computerSelection
+let playerWins = 0; //variable to store wins of the player
+let computerWins = 0; //variable to store wins of the computer
+let tieGames = 0; //variable to store tied games
 
-let playerWins = 0;
-let computerWins = 0;
-let tieGames = 0;
-
-function recordHolder() {
-       let result = playRound(playerSelection, computerSelection);
+function keepScore() { //obtains the result of determineWinner(), stores the results in 3 different variables: playerWins, computerWins, tieGames, and increments them upward accordingly after each round.
+       let result = determineWinner(playerSelection, computerSelection);
         if (result == "You win!") {
             return playerWins++;
         } else if (result == "You lose!"){
@@ -62,24 +58,37 @@ function recordHolder() {
             return tieGames++;
         } 
     }
-
-function game() {
-    for (let i=0; i<4; i++){ //loop game 5 times
-        userPlay(); //ask the user for rock/paper/scissors
-        computerPlay(); //randomly generate computer's choices of rock/paper/scissors
-    }
-}
-
-
-
-console.log(playerSelection); 
+function playRound () {       
+console.log(playerSelection);
 console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
-console.log(recordHolder());
+console.log(determineWinner(playerSelection, computerSelection));
+keepScore();
 console.log('Player wins: ', playerWins);
 console.log('Computer wins: ', computerWins);
 console.log('Tie games: ', tieGames);
-console.log(game());
+} 
+
+function playGame () {
+    for (let i=0; i<=4; i++){ //loop game() 5 times
+        playRound();
+        
+    }    
+}
+
+playRound();
+
+
+// function restartGame () {
+//     let playAgain = window.prompt("Play the next round? Y/n:")
+//     return playAgain.toLowerCase();
+//     if (playAgain == "y" || "yes"){
+//         playRound();
+//     } else if (playAgain == "n" || "no"){
+//         console.log("Thanks for playing!")
+//     }
+// }
+
+
 
 
 
